c621dd3cc0e65f404269f940abda6d012e1cdf81
Kenneth Lee <klee@cognitivescale.com>
2019-05-02 10:31:56 -0500

    syntax error in setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
                       'scikit-learn>=0.20.2, <0.21.0',
                       'scipy>=1.2.1, <1.3.0',
                       'seaborn>=0.9.0, <1.0.0',
-                      'semantic-version=2.3.0, <2.4.0',
+                      'semantic-version>=2.3.0, <2.4.0',
                       'sklearn>=0.0, <0.1.0',
                       'smmap>=0.9.0, <1.0.0',
                       'tensorboard>=1.13.0, <1.14.0',
