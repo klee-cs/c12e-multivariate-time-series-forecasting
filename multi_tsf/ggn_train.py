2f6e8245b07e6e6058a008a78092e47dd9412b3f
Kenneth Lee <klee@cognitivescale.com>
2019-06-06 10:26:57 -0500

    first commit on tf_estimators

--- multi_tsf/ggn_train.py
+++ multi_tsf/ggn_train.py
@@ -5,7 +5,7 @@ from multi_tsf.db_reader import Jackson_GGN_DB
 
 
 def main():
-    epochs = 3000
+    epochs = 100
     batch_size = 128
     conditional = False
     nb_dilation_factors = [1, 2, 4, 8, 16, 32, 64, 128]
@@ -16,13 +16,11 @@ def main():
     predict_hour = 6
 
     jackson_ggn_db = Jackson_GGN_DB(cache_path='./data')
-    skill_ts = jackson_ggn_db.get_summed_work_items_by_skill(start_date='2017-01-31',
-                                                             end_date='2019-03-22',
-                                                             start_hour=0,
-                                                             end_hour=24,
-                                                             include_weekend=False,
-                                                             from_cache=True,
-                                                             skill_list=jackson_ggn_db.default_skills)
+    skill_ts = jackson_ggn_db.filter_active_work_sets(start_date='2017-01-01',
+                                                      end_date='2019-03-23',
+                                                      active_cutoff='2019-01-01',
+                                                      percent_cutoff=90,
+                                                      from_cache=True)
 
 
     skill_ts = pd.DataFrame(skill_ts.iloc[:, 0:3])
